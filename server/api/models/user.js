a05e33d5d9bdd12f6dd1844c3b8126f81bd8b5fc
Pratham <prathamsinghal2003@gmail.com>
2023-07-14 00:06:33 +0530

    feat: add timestamp in model

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -1,24 +1,29 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /^[a-zA-Z0-9]+$/,
+const userSchema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[a-zA-Z0-9]+$/,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^\S+@\S+\.\S+$/,
+    },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /^\S+@\S+\.\S+$/,
-  },
-  password: { type: String, required: true },
-  role: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
