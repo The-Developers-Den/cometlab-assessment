fdc868a1cf19b4328a140bb9bb2487c5ec2ef842
Pratham <prathamsinghal2003@gmail.com>
2023-07-13 13:57:32 +0530

    feat: add monogdb connecting func

---
+++ server/api/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[a-zA-Z0-9]+$/,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^\S+@\S+\.\S+$/,
+  },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const user = require("./api/routes/user");
 const app = express();
 const port = process.env.PORT || 3000;
+
+//connecting mongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to MongoDB!");
+});
 
 app.listen(port, () => {
   console.log(`Server started at port ${port}`);
